62f05dff1f674dc3545054973f9d93f5a6ab4582
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2020-09-08 23:59:21 +0100

    Implemented products for

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'screens/home_screen.dart';
+import 'screens/filter_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(),
+      home: FilterScreen(),
     );
   }
 }
--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class FilterScreen extends StatefulWidget {
   @override
@@ -6,8 +7,148 @@ class FilterScreen extends StatefulWidget {
 }
 
 class _FilterScreenState extends State<FilterScreen> {
+  String selectedProductFor = "Both";
+
+  void selectProductFor(String productFor) {
+    setState(() {
+      selectedProductFor = productFor;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      appBar: AppBar(
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.close,
+            color: Colors.white,
+          ),
+        ),
+        title: Text("Filters"),
+        backgroundColor: Hexcolor('#ff6c70'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+            Icons.check,
+            color: Colors.white,
+          ))
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                border: Border(
+                    bottom:
+                        BorderSide(width: 0.5, color: Colors.grey.shade400))),
+            padding: EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "PRODUCTS FOR",
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade600),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: () {
+                        selectProductFor("Men");
+                      },
+                      child: Row(
+                        children: <Widget>[
+                          (selectedProductFor == "Men")
+                              ? Icon(
+                                  Icons.mail,
+                                  color: Colors.red.shade400,
+                                )
+                              : Icon(
+                                  Icons.mail,
+                                  color: Colors.grey,
+                                ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text(
+                            "Men",
+                            style: TextStyle(
+                                color: (selectedProductFor == "Men")
+                                    ? Colors.red.shade400
+                                    : Colors.grey),
+                          )
+                        ],
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        selectProductFor("Women");
+                      },
+                      child: Row(
+                        children: <Widget>[
+                          (selectedProductFor == "Women")
+                              ? Icon(
+                                  Icons.mail,
+                                  color: Colors.red.shade400,
+                                )
+                              : Icon(
+                                  Icons.mail,
+                                  color: Colors.grey,
+                                ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text("Women",
+                              style: TextStyle(
+                                  color: (selectedProductFor == "Women")
+                                      ? Colors.red.shade400
+                                      : Colors.grey))
+                        ],
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        selectProductFor("Both");
+                      },
+                      child: Row(
+                        children: <Widget>[
+                          (selectedProductFor == "Both")
+                              ? Icon(
+                                  Icons.mail,
+                                  color: Colors.red.shade400,
+                                )
+                              : Icon(
+                                  Icons.mail,
+                                  color: Colors.grey,
+                                ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text("Both",
+                              style: TextStyle(
+                                  color: (selectedProductFor == "Both")
+                                      ? Colors.red.shade400
+                                      : Colors.grey))
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
